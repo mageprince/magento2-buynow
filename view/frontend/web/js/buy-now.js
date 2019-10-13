c3ea573811f9b4098006804a82c309453259207f
Prince Patel <prince.patel@krishtechnolabs.com>
2019-10-13 13:40:01 +0530

    Solve buynow not working with configurable products

--- view/frontend/web/js/buy-now.js
+++ view/frontend/web/js/buy-now.js
@@ -4,12 +4,27 @@ define([
     "use strict";
     return function (config, element) {
         $(element).click(function () {
-            var form = $(config.form);
-            var baseUrl = form.attr('action'),
-                buyNowUrl = baseUrl.replace('checkout/cart/add', 'buynow/cart/add');
+            var form = $(config.form),
+                baseUrl = form.attr('action'),
+                addToCartUrl = 'checkout/cart/add',
+                buyNowCartUrl = 'buynow/cart/add',
+                buyNowUrl = baseUrl.replace(addToCartUrl, buyNowCartUrl);
             form.attr('action', buyNowUrl);
-            form.trigger('submit');
-            form.attr('action', baseUrl);
+            if(form.valid()) {
+                $.ajax({
+                    type: form.attr('method'),
+                    url: form.attr('action'),
+                    data: form.serialize(),
+                    success: function (data) {
+                        form.attr('action', baseUrl);
+                        if (data.hasOwnProperty('backUrl')) {
+                            window.location.href = data.backUrl;
+                        } else {
+                            window.location.reload(true);
+                        }
+                    }
+                });
+            }
             return false;
         });
     }
