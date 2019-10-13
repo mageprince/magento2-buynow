f403ada31a7a361b655ef36da4e42187f8aebbc2
Prince Patel <prince.patel@krishtechnolabs.com>
2019-10-13 12:28:55 +0530

    Set default button title

--- Helper/Data.php
+++ Helper/Data.php
@@ -31,6 +31,26 @@ namespace Prince\Buynow\Helper;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     /**
+     * Buynow button title path
+     */
+    const BUYNOW_BUTTON_TITLE_PATH = 'buynow/general/button_title';
+
+    /**
+     * Buynow button title
+     */
+    const BUYNOW_BUTTON_TITLE = 'Buy Now';
+
+    /**
+     * Addtocart button form id path
+     */
+    const ADDTOCART_FORM_ID_PATH = 'buynow/general/addtocart_id';
+
+    /**
+     * Addtocart button form id
+     */
+    const ADDTOCART_FORM_ID = 'product_addtocart_form';
+
+    /**
      * Retrieve config value
      *
      * @return string
@@ -41,5 +61,25 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             $config,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
+    }
+
+    /**
+     * Get button title
+     * @return string
+     */
+    public function getButtonTitle()
+    {
+        $btnTitle = $this->getConfig(self::BUYNOW_BUTTON_TITLE_PATH);
+        return $btnTitle ? $btnTitle : self::BUYNOW_BUTTON_TITLE;
+    }
+
+    /**
+     * Get addtocart form id
+     * @return string
+     */
+    public function getAddToCartFormId()
+    {
+        $addToCartFormId = $this->getConfig(self::ADDTOCART_FORM_ID_PATH);
+        return $addToCartFormId ? $addToCartFormId : self::ADDTOCART_FORM_ID;
     }
 }
